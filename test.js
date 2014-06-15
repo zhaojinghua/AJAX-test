aca58c676a628cd164f77ce38ff18f0ff33ded5a
zhaojinghua <zhaojinghua@hotmail.com>
2014-06-15 15:04:20 -0700

    ajax change innerhtml by id

--- test.js
+++ test.js
@@ -2,7 +2,8 @@ var request = new XMLHttpRequest();
 request.open("GET", "data.txt");
 request.onreadystatechange = function () {
 	if (request.readyState == 4 && request.status == 200){				
-		document.write(request.responseText);
+		var modify = document.getElementById("update");
+		modify.innerHTML = request.responseText;
 	}
 };
 request.send();
