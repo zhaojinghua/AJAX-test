28856ad9a22a0b99557775f495804cd0973f915e
zhaojinghua <zhaojinghua@hotmail.com>
2014-06-15 16:04:13 -0700

    no obvious change

--- test.js
+++ test.js
@@ -3,11 +3,11 @@ request.open("GET", "data.json");
 request.onreadystatechange = function () {
 	if (request.readyState == 4 && request.status == 200){
 			var items = JSON.parse(request.responseText);				
-			var output = "<ul>"
+			var output = "<ul>";
 			for(var key in items){
-				output += "<li>" + items[key].name + "</li>"	
+				output += "<li>" + items[key].name + "</li>";	
 			}
-			output += "</ul>"
+			output += "</ul>";
 			document.getElementById("update").innerHTML = output;
 	}
 };
